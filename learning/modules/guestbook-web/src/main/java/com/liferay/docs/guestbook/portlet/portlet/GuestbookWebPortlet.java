724e0058973d71d835b1b35a1e5866905fbaa937
phamducdat <phamducdat2402@gmail.com>
2022-09-16 19:23:36 +0700

    init

--- learning/modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
+++ learning/modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookWebPortlet.java
@@ -23,15 +23,17 @@ import java.util.logging.Logger;
 @Component(
 	immediate = true,
 	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=GuestbookWeb",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + GuestbookWebPortletKeys.GUESTBOOKWEB,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+			"com.liferay.portlet.display-category=category.social",
+			"com.liferay.portlet.instanceable=false",
+			"com.liferay.portlet.scopeable=true",
+			"javax.portlet.display-name=Guestbook",
+			"javax.portlet.expiration-cache=0",
+			"javax.portlet.init-param.template-path=/",
+			"javax.portlet.init-param.view-template=/guestbookwebportlet/view.jsp",
+			"javax.portlet.resource-bundle=content.Language",
+			"javax.portlet.security-role-ref=power-user,user",
+			"javax.portlet.supports.mime-type=text/html",
+			"javax.portlet.name=" + GuestbookWebPortletKeys.GUESTBOOKWEB,
 	},
 	service = Portlet.class
 )
