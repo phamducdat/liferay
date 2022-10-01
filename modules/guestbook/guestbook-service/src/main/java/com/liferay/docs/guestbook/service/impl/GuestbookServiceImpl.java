0f1b6cf927c443d21703773aed3a497260205785
phamducdat <phamducdat2402@gmail.com>
2022-10-01 15:17:50 +0700

    update

--- modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookServiceImpl.java
+++ modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookServiceImpl.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -14,20 +14,47 @@
 
 package com.liferay.docs.guestbook.service.impl;
 
+import com.liferay.docs.guestbook.model.Guestbook;
 import com.liferay.docs.guestbook.service.base.GuestbookServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
-
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
 import org.osgi.service.component.annotations.Component;
+
+import java.util.List;
 
 /**
  * @author liferay
  */
 @Component(
-	property = {
-		"json.web.service.context.name=gb",
-		"json.web.service.context.path=Guestbook"
-	},
-	service = AopService.class
+        property = {
+                "json.web.service.context.name=gb",
+                "json.web.service.context.path=Guestbook"
+        },
+        service = AopService.class
 )
 public class GuestbookServiceImpl extends GuestbookServiceBaseImpl {
+
+    public Guestbook addGuestbook(long userId, String name,
+                                  ServiceContext serviceContext) throws PortalException {
+        return guestbookLocalService.addGuestbook(userId, name, serviceContext);
+    }
+
+    public List<Guestbook> getGuestbooks(long groupId) {
+        return guestbookLocalService.getGuestbooks(groupId);
+    }
+
+    public List<Guestbook> getGuestbooks(long groupId, int start, int end) {
+        return guestbookLocalService.getGuestbooks(groupId, start, end);
+    }
+
+    public int getGuestbooksCount(long groupId) {
+        return guestbookLocalService.getGuestbooksCount();
+    }
+
+    public Guestbook updateGuestbook(long userId, long guestbookId,
+                                     String name, ServiceContext serviceContext) throws PortalException {
+        return guestbookLocalService.updateGuestbook(userId, guestbookId,
+                name, serviceContext);
+    }
 }
